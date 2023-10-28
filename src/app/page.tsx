bd4ce35afd47e9a110c2d177f8c0a88adf0248f2
Ben Alderfer <ben.alderfer@gmail.com>
2023-10-28 00:05:30 -0700

    Make site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,86 @@
 import Image from 'next/image'
+import OpenAI from "openai"
+import { Suspense } from 'react'
+import Markdown from 'react-markdown'
 
-export default function Home() {
+const INTRO = "You’re writing a book. It’s a period story about a time in Ben’s late 20s/early 30s. At the beginning he’s new to living alone in his first 1 bdrm apartment. He has a girlfriend and a dog named Bailey. Across the hall he here’s of neighbors roughly his age who host a party every few weeks. He wants to figure out some way to work himself into that entirely foreign friend group. By the end of the novel he’s living happily with his girlfriend and some of their best friends have come out of that once foreign friend group.\n\nWhat are the 12 chapters of the book?"
+const INITIAL_MESSAGES: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  {"role": "system", "content": "You are a helpful writing assistant that only produces content in markdown format."},
+  {"role": "user", "content": INTRO},
+]
+const FOLLOW_UPS = [
+  "Write chapter 1",
+  "Write chapter 2",
+  "Write chapter 3",
+  "Write chapter 4",
+  "Write chapter 5",
+  "Write chapter 6",
+  "Write chapter 7",
+  "Write chapter 8",
+  "Write chapter 9",
+  "Write chapter 10",
+  "Write chapter 11",
+  "Write chapter 12",
+]
+
+async function getData(messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[]) {
+  console.log(process.env.OPENAI_API_KEY)
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
+  
+  console.log("CALLING CHATGPT WITH MESSAGES:\n\n", messages)
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages,
+  });
+  console.log("RECEIVED RESPONSE:\n\n", response)
+  // The return value is *not* serialized
+
+  // You can return Date, Map, Set, etc.
+ 
+  return response
+}
+
+async function NextMessages({ messages, count }: { count: number, messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] }) {
+  const data = await getData([...messages, {"role": "user", "content": FOLLOW_UPS[count]}]);
+
+  const content = data.choices[0].message.content
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Markdown>{content}</Markdown>
+      {count < 11 ? (
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <Suspense fallback={<div>loading...</div>}>
+        <NextMessages count={count + 1} messages={
+          [
+            ...messages,
+            {"role": "system", "content": content},
+            
+          ]
+        } />
+      </Suspense>
+        ) : <div/>}
+    </>
+  )
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default async function Home() {
+  const data = await getData(INITIAL_MESSAGES)
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  const content = data.choices[0].message.content
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  console.log(data)
+  return (
+    <main className="p-24">
+      <div className="prose lg:prose-xl">
+        <Markdown>{content}</Markdown>
+        <Suspense fallback={<div>loading...</div>}>
+          <NextMessages count={0} messages={[
+            ...INITIAL_MESSAGES,
+            {"role": "system", "content": content},
+          ]}/>
+        </Suspense>
       </div>
     </main>
   )
